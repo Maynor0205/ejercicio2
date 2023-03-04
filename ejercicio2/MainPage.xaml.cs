0fcd07b1d36d9c61413519a09bde2b082dcb4ff4
Maynor Martinez <Maynor Martinez@MAYNOR_VASQUEZ>
2023-03-04 11:21:04 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ejercicio2.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnprocesar_Clicked(object sender, EventArgs e)
+        {
+            var emple = new Empleado
+            {
+                id = 1,
+                nombres = txtnombre.Text,
+                apellidos = txtapellido.Text,
+                edad = int.Parse(txtedad.Text),
+                correo = txtacorreo.Text,
+                foto = null
+            };
+
+            // Esto es para llamar una segunda pagina y pasarle parametro a una segunda pantalla
+            var secondpage = new PageResultado();
+            secondpage.BindingContext = emple; // Paso de parametros a la pantalla
+            Navigation.PushAsync(secondpage);
+        }
+    }
+}
---
+++ ejercicio2/models/Empleado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejercicio2.models
+{
+    internal class Empleado
+    {
+        public int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public int edad { get; set; }
+        public string correo { get; set; }
+        public string foto { get; set; }
+    }
+}
